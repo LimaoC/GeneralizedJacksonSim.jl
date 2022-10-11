b84b9a5f3ad9080545deacbb767ca213dc0ed8fc
LimaoC <80520563+LimaoC@users.noreply.github.com>
2022-10-11 21:44:14 +1000

    task2: finish task2, add some utility functions to src/GeneralizedJacksonSim.jl

--- src/GeneralizedJacksonSim.jl
+++ src/GeneralizedJacksonSim.jl
@@ -1,3 +1,65 @@
+"""
+A discrete event simulation engine for Open Generalized Jackson Networks.
+"""
 module GeneralizedJacksonSim
+
+using Parameters
+using Accessors
+using LinearAlgebra
+using Random
+
+export NetworkParameters, maximal_alpha_scaling, set_scenario, compute_ρ
+
+"""
+    NetworkParameters
+
+# Fields
+- `L::Int`: the dimension of the network (number of nodes)
+- `α_vector::Vector{Float64}`: the external arrival rates α_i >= 0
+- `μ_vector::Vector{Float64}`: the service rates μ_i > 0
+- `P::Matrix{Float64}`: the L×L routing matrix P
+- `cs::Float64`: squared coefficient of variation of the service processes, defaults to 1.0
+"""
+@with_kw struct NetworkParameters
+    L::Int
+    α_vector::Vector{Float64}
+    μ_vector::Vector{Float64}
+    P::Matrix{Float64}
+    c_s::Float64 = 1.0
+end
+
+"""
+    maximal_alpha_scaling(net::NetworkParameters)
+
+Computes the maximal value by which we can scale the network's α_vector and be stable.
+"""
+function maximal_alpha_scaling(net::NetworkParameters)
+    λ_base = (I - net.P') \ net.α_vector  # solve the traffic equations
+    ρ_base = λ_base ./ net.μ_vector  # determine the load ρ  
+    return minimum(1 ./ ρ_base)
+end
+
+"""
+    set_scenario(net::NetworkParameters, ρ::Float64, c_s::Float64 = 1.0)
+
+Adjusts the network parameters to the desired ρ⋆ and c_s.
+"""
+function set_scenario(net::NetworkParameters, ρ::Float64, c_s::Float64=1.0)
+    (ρ ≤ 0 || ρ ≥ 1) && error("ρ is out of range")
+    max_scaling = maximal_alpha_scaling(net)
+    net = @set net.α_vector = net.α_vector * max_scaling * ρ
+    net = @set net.c_s = c_s
+    return net
+end
+
+"""
+    compute_ρ(net::NetworkParameters)
+
+Computes the vector of ρ values.
+"""
+function compute_ρ(net::NetworkParameters)
+    λ = (I - net.P') \ net.α_vector  # solve traffic equations
+    return λ ./ net.μ_vector  # vector of ρ values
+end
 
 end
