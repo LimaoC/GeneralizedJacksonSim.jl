3baf103dcac9854adb1fa9e4c1133d516d800fd2
LimaoC <80520563+LimaoC@users.noreply.github.com>
2022-10-12 20:11:13 +1000

    task3: implement process_event() for EndOfServiceEvent
    - Removed unnecessary ArrivalEvent struct

--- src/event.jl
+++ src/event.jl
@@ -6,14 +6,6 @@ This file contains the Event implementation and related functionality.
 abstract type Event end
 
 """
-An event that signifies an arrival from inside the system (i.e., another server) to the
-server `q`.
-"""
-@with_kw struct ArrivalEvent <: Event
-    q::Int = 1
-end
-
-"""
 An event that signifies an arrival from outside the system to the server `q`.
 """
 @with_kw struct ExternalArrivalEvent <: Event
@@ -51,7 +43,8 @@ isless(te1::TimedEvent, te2::TimedEvent) = te1.time < te2.time
 """
     process_event(time::Float64, state::State, event::ExternalArrivalEvent)
 
-Process an arrival event from outside the system.
+Process an arrival event from outside the system, and spawns a list of events that occur as
+a consequence of this arrival.
 
 On arrival, if the server is free (no jobs in the buffer/queue), the job starts to receive
 service. If the server is busy, the job queues for service and waits for its turn.
@@ -66,12 +59,55 @@ function process_event(time::Float64, state::State, event::ExternalArrivalEvent)
 
     # prepare next external arrival for this particular server
     push!(new_timed_events,
-          TimedEvent(ExternalArrivalEvent(q), time + next_arrival_time(state, q)))
+        TimedEvent(ExternalArrivalEvent(q), time + next_arrival_time(state, q)))
 
     # start serving this job if it is the only one in the queue
     if state.queues[q] == 1
         push!(new_timed_events,
-              TimedEvent(EndOfServiceEvent(q), time + next_service_time(state, q)))
+            TimedEvent(EndOfServiceEvent(q), time + next_service_time(state, q)))
+    end
+    return new_timed_events
+end
+
+"""
+    process_event(time::Float64, state::State, event::EndOfServiceEvent)
+
+Process an end-of-service event, and spawns a list of events that occur as a consequence of
+this end of service.
+
+When a job completes service at a buffer, it either leaves the system, or moves to another
+buffer (both happen immediately). After completing service in server i, a job moves to
+server j with probability P[i, j], where P is the routing matrix.
+"""
+function process_event(time::Float64, state::State, event::EndOfServiceEvent)
+    q = event.q
+    state.queues[q] -= 1  # remove from queue
+    @assert state.queues[q] >= 0
+    new_timed_events = TimedEvent[]
+
+    # if there is another customer in the queue, start serving them
+    if state.queues[q] > 0
+        push!(new_timed_events,
+            TimedEvent(EndOfServiceEvent(q), time + next_service_time(state, q)))
+    end
+
+    # simulate the next location for this job; indices 1:L are the probabilities of moving
+    # to another server in the system, and the last index is the probability of exiting
+    # the system
+    L = state.net.L
+    next_loc_weights = state.net.P[q, :]
+    push!(next_location_weights, 1 - sum(next_location_weights))
+    next_loc = sample(1:L+1, Weights(next_location_weights))
+
+    if next_loc <= L
+        # job is staying in the system
+        state.queues[next_loc] += 1
+
+        # start serving job if it is the only one in the queue
+        if state.queues[next_loc] == 1
+            push!(new_timed_events,
+                TimedEvent(EndOfServiceEvent(q), time + next_service_time(state, q)))
+        end
     end
     return new_timed_events
 end
