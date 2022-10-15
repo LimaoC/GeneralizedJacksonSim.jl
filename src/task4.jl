680e85a23f92836a66b542820d3c2338fcfdd4b9
Tiarne-Graves <109961226+Tiarne-Graves@users.noreply.github.com>
2022-10-15 20:11:32 +1000

    task 4, all 4 graphs

--- src/task4.jl
+++ src/task4.jl
@@ -29,50 +29,21 @@ function plot_mean_queue_length_service_times(net::NetworkParameters, scenario_n
         end
 
         push!(plotting_data, simulated_total_mean_queue_lengths)
-        # #generating plot for each service time ##below is creating all of the plots, so need to alter this so that its on one plots
-        # push! = plot!(plotter, plot(
-        #     ρ_star_values,
-        #     simulated_total_mean_queue_lengths,
-        #     xlabel="ρ",
-        #     ylabel="Total Mean Queue Length",
-        #     title="Scenario $scenario_number Simulation"))
     end
-    @show plotting_data
-
-    p1 = plot(ρ_star_values, plotting_data[1])
-    p2 = plot!(ρ_star_values, plotting_data[2])
-    p3 = plot(ρ_star_values, plotting_data[3])
-    p4 = plot(ρ_star_values, plotting_data[4])
-    p5 = plot(ρ_star_values, plotting_data[5])
     
     return plot(ρ_star_values,[plotting_data[1],plotting_data[2],plotting_data[3],plotting_data[4],plotting_data[5]], xlabel="ρ",
         ylabel="Total Mean Queue Length",
-        title="Scenario $scenario_number Simulation")
+        title="Scenario $scenario_number Simulation with varying service times",
+        labels=["0.1" "0.5" "1.0" "2.0" "4.0"])
 
-    # f = plot(plots[1])
-    # g = plot!(f,plots[2])
-# return p1
-    # return g
-    # return plot!(plots[1],plots[2],plots[3],plots[4],plots[5],legend=true, size=(1000, 1000))
-    # return plot(plots..., legend=true, size=(1000, 1000)) ##needs to combine all plots for service times from above onto one figure
+
     end
-
-    # plots = Vector()
-    # for (scenario_number, scenario) in enumerate(scenarios)
-    #     push!(plots, test_sim(scenario,
-    #                           scenario_number,
-    #                           verbose=verbose,
-    #                           multithreaded=multithreaded)...)
-    # end
-    # plot(plots..., layout=(length(scenarios), 2), legend=true, size=(1000, 1000))
-
-
 
 
 
 p1 = plot_mean_queue_length_service_times(scenario1, 1)
-# p2 = plot_mean_queue_length_service_times(scenario2, 2)
-# p3 = plot_mean_queue_length_service_times(scenario3, 3)
-# p4 = plot_mean_queue_length_service_times(scenario4, 4)
+p2 = plot_mean_queue_length_service_times(scenario2, 2)
+p3 = plot_mean_queue_length_service_times(scenario3, 3)
+p4 = plot_mean_queue_length_service_times(scenario4, 4)
         
-# plot(p1, p2, p3, p4, layout=(2, 2), legend=false, size=(800, 800))
+plot(p1, p2, p3, p4, layout=(2, 2), legend=true, size=(800, 800))
