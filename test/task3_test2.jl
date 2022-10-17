719478952a1f0d38799559c1b558f8492ebe8925
LimaoC <80520563+LimaoC@users.noreply.github.com>
2022-10-17 18:05:58 +1000

    task3: add varying c_s values for test 2

--- test/task3_test2.jl
+++ test/task3_test2.jl
@@ -11,24 +11,29 @@ Prints the simulated average number of arrivals (rounded to `digits` decimal pla
 the theoretical average number of arrivals.
 """
 function task3_test2(scenarios::Vector{NetworkParameters};
-                     max_time::Int64=10^6, digits::Int64=3)
+                     max_time::Int64=10^5, digits::Int64=3,
+                     c_s_values::Vector{Float64}=[0.1, 0.5, 1.0, 2.0, 4.0])
     for (index, scenario) in enumerate(scenarios)
         println("Scenario $index:")
-        # pass in a state to sim_net() so we have a reference to the state to calculate the
-        # mean number of arrivals
-        state = QueueNetworkState(scenario)
-        _ = sim_net(scenario, state=state, max_time=max_time)
+        for c_s in c_s_values
+            println("   c_s = $c_s:")
+            scenario = @set scenario.c_s = c_s
+            # pass in a state to sim_net() so we have a reference to the state to calculate the
+            # mean number of arrivals
+            state = QueueNetworkState(scenario)
+            _ = sim_net(scenario, state=state, max_time=max_time)
 
-        # calculate mean number of arrivals
-        mean_num_arrivals = state.arrivals ./ max_time
-        theoretical_mean_num_arrivals = (I - scenario.P') \ scenario.α_vector
-        sum_of_squares = sum((mean_num_arrivals - theoretical_mean_num_arrivals).^2)
+            # calculate mean number of arrivals
+            mean_num_arrivals = state.arrivals ./ max_time
+            theoretical_mean_num_arrivals = (I - scenario.P') \ scenario.α_vector
+            sum_of_squares = sum((mean_num_arrivals - theoretical_mean_num_arrivals).^2)
 
-        println("Simulated average number of arrivals  : " *
-                "$(round.(mean_num_arrivals, digits=digits))")
-        println("Theoretical average number of arrivals: " *
-                "$(round.(theoretical_mean_num_arrivals, digits=digits))")
-        println("Sum of squared differences            : " *
-                "$(round.(sum_of_squares, digits=digits))")
+            println("       Simulated average number of arrivals  : " *
+                    "$(round.(mean_num_arrivals, digits=digits))")
+            println("       Theoretical average number of arrivals: " *
+                    "$(round.(theoretical_mean_num_arrivals, digits=digits))")
+            println("       Sum of squared differences            : " *
+                    "$(round.(sum_of_squares, digits=digits))")
+        end
     end
 end
